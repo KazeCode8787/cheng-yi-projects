56c715fdab8fb9ff961fd70d11613c32afd7a6aa
tobycoding0501 <tobycoding0501@gmail.com>
2022-09-30 10:19:20 +0800

    930

---
+++ 0930.py
@@ -0,0 +1,16 @@
+import math
+
+print(math.radians(90))
+print(max(10,8,-9,-100,77,50,28))
+print(min(10,8,-9,-100,77,50,28))
+print(10*10*math.pi)
+print(math.cos(math.radians(60)))
+print(math.sqrt(7))
+print(math.gcd(616,1331))
+
+x = math.log(2,10)
+y = math.log(3,10)
+
+result = pow(10,2*x+3*y+1)
+print("結果為：",result)
+
