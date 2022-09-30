4fcc7a1c0f416573ffe110ac984c623fb660fb4a
tobycoding0501 <tobycoding0501@gmail.com>
2022-09-30 11:16:12 +0800

    930-2

--- 0930.py
+++ 0930.py
@@ -1,5 +1,6 @@
 import math
 import random as r
+m = math
 
 print(math.radians(90))
 print(max(10,8,-9,-100,77,50,28))
@@ -25,3 +26,11 @@ L = [1,2,3,4,5]
 r.shuffle(L)
 print(L)
 print(r.choice(L))
+
+num = r.randint(1,3)
+answer = eval(input("猜數字(1~3)："))
+print(num,"==",answer,"is",num==answer)
+
+s =eval(input("請輸入正六邊形的邊長："))
+area = 6*s*s/(4*m.tan(m.pi/6))
+print("邊長",s,"的正六邊形面積：",area)
