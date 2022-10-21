d1f5336b1af4a453965ffa16a9dfdc29a2446961
tobycoding0501 <tobycoding0501@gmail.com>
2022-10-21 10:28:22 +0800

    ok

---
+++ 1021.py
@@ -0,0 +1,18 @@
+s1 = "HappyNewYear"
+s2 = "happynewyear"
+s3 = "new"
+
+print(len(s1))
+print(s1 == s2)
+print(max(s1))
+print(s3 in s1)
+print(s1[4:9])
+
+x = "Hello, World"
+print(x.upper())
+print(str.upper(x))
+print(x.lower())
+print(x.swapcase())
+print(x.replace("World", "Tim"))
+print(str.capitalize("an egg"))
+print(str.title("an egg"))
