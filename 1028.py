b0ee4aeecd70dacfc351c6b9ae486e2d4b2efc8b
tobycoding0501 <tobycoding0501@gmail.com>
2022-10-28 10:42:40 +0800

    sus

---
+++ 1028.py
@@ -0,0 +1,16 @@
+top, bottom, height = 10, 20, 5
+print("梯形的上底{}cm, 下底{}cm, 高{}cm".format(top,bottom,height))
+s1 = "\nMerry\tChristmas!\n"
+print(s1.strip())
+print(s1)
+s4 = "Monday"
+s5 = "monday"
+print(s4.center(30))
+s6 = s4.upper()
+print(s6)
+s7 = s4.lower()
+print(s7)
+s8 = s5.replace("mon","Fri")
+print(s4.isdigit())
+print(s4.endswith('day'))
+print(s4.find('o'))
