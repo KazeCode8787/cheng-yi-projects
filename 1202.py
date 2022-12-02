f1f714205a5aaed2f2b194ef0d97e33ddb01b81e
tobycoding0501 <tobycoding0501@gmail.com>
2022-12-02 11:24:19 +0800

    ok

--- 1202.py
+++ 1202.py
@@ -1,3 +1,4 @@
+import random as ra
 result1, result2 = '',''
 
 for i in range(1,10):
@@ -18,3 +19,38 @@ n = eval(input("請輸入金字塔的高度(1~30)："))
 
 for i in range(1,n+1):
     print(f"{' '*(n-i)}","*"*(2*i-1))
+
+ans = input("請輸入\"快樂\"的英文")
+
+while ans.upper()!= "HAPPY":
+    ans = input("答錯了")
+else:
+    print("答對了")
+
+num = eval(input("請輸入1~10的正整數"))
+
+result = 1
+i = 1
+while i <= num:
+    result *= i
+    i+=1
+print(f"{num}乘階為{result}")
+
+
+num = ra.randint(1,10)
+ans = -1
+while ans != num:
+    ans = eval(input("請猜數字1~10"))
+    if ans > num:
+        print("Too big")
+    elif ans<num:
+        print("Too small")
+    else:
+        print("Correct")
+
+money = float(25000.0)
+years = 0
+while money < 50000:
+    money *= 1.03
+    years += 1
+print(years)
