6d22e77b6fc8b65d0bbfa7f77f79bbe8326d2326
tobycoding0501 <tobycoding0501@gmail.com>
2022-12-09 10:31:06 +0800

    ok

---
+++ 1209.py
@@ -0,0 +1,34 @@
+for i in range(2,100):
+    for j in range(2,i):
+        if i % j == 0:
+            break
+    else:
+        print(i,end='\t')
+
+print("")
+number = int(input())
+current = 2
+keys = set()
+temp = {}
+while True:
+    if number == current:
+        if current in keys:
+            temp[current] += 1
+        else:
+            keys.add(current)
+            temp[current] = 1
+        break
+    if number%current==0:
+        number/=current
+        if current in keys:
+            temp[current] += 1
+        else:
+            keys.add(current)
+            temp[current] = 1
+    else:
+        current = current+1 if current==2 else current+2
+
+string = ""
+for i in temp:
+    string += f"{i}({temp[i]})*"
+print(string)
