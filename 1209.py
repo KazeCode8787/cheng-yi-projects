40759da2ca7d16169df56065ceb63a90cd674353
tobycoding0501 <tobycoding0501@gmail.com>
2022-12-09 11:16:10 +0800

    ok

--- 1209.py
+++ 1209.py
@@ -39,3 +39,21 @@ string = ""
 for i in temp:
     string += f"{i}({temp[i]})*"
 print(string[:-1])
+
+def CtoF1(degreeC):
+    print("攝氏",degreeC,"度可以轉換成華氏",degreeC*1.8+32,"度")
+
+tC = eval(input("Enter degreeC:"))
+CtoF1(tC)
+
+def aver(*nums):
+    return sum(nums)/len(nums)
+
+def geo(*nums):
+    ret = 1
+    for i in nums:
+        ret *= i
+    return pow(ret,1/len(nums))
+
+print(aver(1,4,5,6,7,3,8,4,9))
+print(geo(1,4,5,6,7,3,8,4,9))
