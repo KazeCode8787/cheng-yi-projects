6dad1c9513fb76836bea373766767b29fc57cd82
tobycoding0501 <tobycoding0501@gmail.com>
2022-12-30 11:50:43 +0800

    sus

--- temp.py
+++ temp.py
@@ -1,29 +1,30 @@
-v,n = map(int,input().split())    # x=input().split()
-# b=10
-# c=0
-# v=int(x[0])
-# n=int(x[1])
+# v,n = map(int,input().split())    # x=input().split()
+# # b=10
+# # c=0
+# # v=int(x[0])
+# # n=int(x[1])
 
-for a in range(v,n+1):
-    b = 10      
-    c = 0
-    j = len(str(a))
-    at = a
-    while a>0:      #     while a//b>0:
-        d=a%b       #         d=a%b
-        e=d**j      #         e=d**4
-        c=c+e       #         c=c+e
-        a//=10      #         b=10*b
-    if c==at:       #         if c==a:
-        print(at)   #             print(a)
+# for a in range(v,n+1):
+#     b = 10      
+#     c = 0
+#     j = len(str(a))
+#     at = a
+#     while a>0:      #     while a//b>0:
+#         d=a%b       #         d=a%b
+#         e=d**j      #         e=d**4
+#         c=c+e       #         c=c+e
+#         a//=10      #         b=10*b
+#     if c==at:       #         if c==a:
+#         print(at)   #             print(a)
 
 
 
 v,n=map(int,input().split())    # x=input().split()
-b=10
-c=0
+
 
 for a in range(v,n+1):
+    b=10
+    c=0
     while a//b>0:
         d=a%b
         e=d**4
@@ -31,6 +32,5 @@ for a in range(v,n+1):
         b=10*b
     if c==a:       
         print(a)   
-
 
 
