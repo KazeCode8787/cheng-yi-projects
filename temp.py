950d0a531948533907542559b288704d72e58ede
TobyCoding0501 <tobycoding0501@gmail.com>
2023-01-03 22:58:00 +0800

    a

--- temp.py
+++ temp.py
@@ -1,36 +1,5 @@
-# v,n = map(int,input().split())    # x=input().split()
-# # b=10
-# # c=0
-# # v=int(x[0])
-# # n=int(x[1])
-
-# for a in range(v,n+1):
-#     b = 10      
-#     c = 0
-#     j = len(str(a))
-#     at = a
-#     while a>0:      #     while a//b>0:
-#         d=a%b       #         d=a%b
-#         e=d**j      #         e=d**4
-#         c=c+e       #         c=c+e
-#         a//=10      #         b=10*b
-#     if c==at:       #         if c==a:
-#         print(at)   #             print(a)
+def cmd(a,*,b=0,c=0,**d):
+    print(d)
 
 
-
-v,n=map(int,input().split())    # x=input().split()
-
-
-for a in range(v,n+1):
-    b=10
-    c=0
-    while a//b>0:
-        d=a%b
-        e=d**4
-        c=c+e
-        b=10*b
-    if c==a:       
-        print(a)   
-
-
+cmd(0,ab=0)
