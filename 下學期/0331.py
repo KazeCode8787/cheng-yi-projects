3562ed7875ad610f21b5e9201b9f1cde3a7ab878
tobycoding0501 <tobycoding0501@gmail.com>
2023-03-31 13:39:39 +0800

    auto upload

---
+++ 下學期/0331.py
@@ -0,0 +1,9 @@
+with open("C://users/user/desktop/sus.txt","w") as f:
+    f.write("登金陵鳳凰台")
+
+with open("C://users/user/desktop/sus.txt","a") as f:
+    f.write("\n鳳凰臺上鳳凰遊，鳳去臺空江自流。")
+    f.write("\n吳宮花草埋幽徑，晉代衣冠成古丘。")
+    f.write("\n三山半落青天外，二水中分白鷺洲。")
+    f.write("\n總為浮雲能蔽日，長安不見使人愁。")
+
