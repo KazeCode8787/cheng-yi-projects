23567f24e3ac8a3878e81574028d4c3104fffc8e
tobycoding0501 <tobycoding0501@gmail.com>
2023-04-07 13:14:23 +0800

    auto upload

--- 下學期/0331.py
+++ 下學期/0331.py
@@ -1,9 +1,12 @@
-with open("C://users/user/desktop/sus.txt","w") as f:
-    f.write("登金陵鳳凰台")
+f = open("C://users/user/desktop/sus.txt","w")
 
-with open("C://users/user/desktop/sus.txt","a") as f:
-    f.write("\n鳳凰臺上鳳凰遊，鳳去臺空江自流。")
-    f.write("\n吳宮花草埋幽徑，晉代衣冠成古丘。")
-    f.write("\n三山半落青天外，二水中分白鷺洲。")
-    f.write("\n總為浮雲能蔽日，長安不見使人愁。")
+f.write("登金陵鳳凰台")
+f.close()
 
+
+f = open("C://users/user/desktop/sus.txt","a")
+f.write("\n鳳凰臺上鳳凰遊，鳳去臺空江自流。")
+f.write("\n吳宮花草埋幽徑，晉代衣冠成古丘。")
+f.write("\n三山半落青天外，二水中分白鷺洲。")
+f.write("\n總為浮雲能蔽日，長安不見使人愁。")
+f.close()
