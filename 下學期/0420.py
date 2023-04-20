9c0967f73af2dc8f6ec4f67fadfff49fa1a502e5
tobycoding0501 <tobycoding0501@gmail.com>
2023-04-20 13:26:27 +0800

    auto upload

---
+++ 下學期/0420.py
@@ -0,0 +1,13 @@
+try:
+    x = eval(input("請輸入被除數X:"))
+    Y = eval(input("請輸入除數Y:"))
+    z = x/Y
+except ZeroDivisionError:
+    print("除數不得為零")
+except Exception as e1:
+    print(e1.args)
+else:
+    print("沒有捕捉到例外!x除以Y的結果等於",z)
+
+finally:
+    print("Leaving try...except scope.")
