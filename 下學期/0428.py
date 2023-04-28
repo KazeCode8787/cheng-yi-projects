597974cd7a9c13a27c29e6a9bbb6e2bd7b2e975d
tobycoding0501 <tobycoding0501@gmail.com>
2023-04-28 13:30:27 +0800

    auto upload

---
+++ 下學期/0428.py
@@ -0,0 +1,59 @@
+class A:
+    x=1
+
+class B(A):
+    y=2
+
+class C(B):
+    z=3
+
+
+
+
+
+obj = C()
+print(f"x的屬性值為 {obj.x}")
+print(f"y的屬性值為 {obj.y}")
+print(f"z的屬性值為 {obj.z}")
+
+# class A:
+#     x = 
+class B:
+    y = 2
+
+class C(A,B):
+    z = 3
+
+obj = C()
+print(f"x的屬性值為 {obj.x}")
+print(f"y的屬性值為 {obj.y}")
+print(f"z的屬性值為 {obj.z}")
+
+class shoppingcar():
+    def __init__(self,owner) -> None:
+        self.__owner = owner
+        self.__product = []
+
+    def getOwner(self):
+        return self.__owner
+
+    def addProduct(self,product):
+        self.__product.append(product)
+
+    def removeProduct(self,product):
+        self.__product.remove(product)
+
+    def getProduct(self):
+        return self.__product
+
+
+obj = shoppingcar("dogg")
+
+obj.addProduct("abc")
+obj.addProduct("sss")
+obj.addProduct("aaa")
+obj.addProduct("www")
+obj.addProduct("qqq")
+obj.addProduct("sdsd")
+obj.removeProduct("abc")
+print(f"{obj.getOwner()} 的購物車內有 {obj.getProduct()}")
