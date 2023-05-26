19f19c5d5b40c1f7e8b16305d724bd3d657e9786
tobycoding0501 <tobycoding0501@gmail.com>
2023-05-26 13:58:56 +0800

    auto upload

--- others/matrix.py
+++ others/matrix.py
@@ -10,5 +10,68 @@ class matrix:
     def __getitem__(self,index):
         return self.__data[index]
     
-    def addminus(self,target,act):
-        pass
+    def __add__(self,target):
+        if target.col == self.col and target.row == self.row:
+            for i in range(self.col):
+                for j in range(self.row):
+                    self[i][j] += target[i][j]
+
+        else:
+            raise BaseException()
+        
+        return self
+    
+    def __sub__(self,target):
+        if target.col == self.col and target.row == self.row:
+            for i in range(self.col):
+                for j in range(self.row):
+                    self[i][j] -= target[i][j]
+
+        else:
+            raise BaseException()
+        
+        return self
+        
+    
+    def __mul__(self,target):
+        if type(target) == int:
+            for i in range(self.col):
+                for j in range(self.row):
+                    self[i][j] *= target
+            return self
+        a,b,b2,c = [self.col,self.row,target.col,target.row]
+        if b != b2:
+            raise BaseException()
+        temp = matrix(a,c)
+
+        for i in range(a):
+            for j in range(c):
+                for k in range(b):
+                    temp[i][j] += self[i][k]*target[k][j]
+
+        return temp
+    
+    def __rmul__(self,target):
+        if type(target) == int:
+                for i in range(self.col):
+                    for j in range(self.row):
+                        self[i][j] *= target
+                return self
+        a,b,b2,c = [self.col,self.row,target.col,target.row]
+        if b != b2:
+            raise BaseException()
+        temp = matrix(a,c)
+
+        for i in range(a):
+            for j in range(c):
+                for k in range(b):
+                    temp[i][j] += self[i][k]*target[k][j]
+
+        return temp
+    def __setitem__(self,index,val):
+        self.__data[index] = val
+
+    def __iter__(self):
+        for i_ in range(self.col):
+            for j_ in range(self.row):
+                yield self.__data[i_][j_]
