e779e5de78fab0ca1e46cee48b3dec5bcb3be429
TobyCoding0501 <tobycoding0501@gmail.com>
2023-06-12 13:42:12 +0800

    auto upload

---
+++ final exam/a059.py
@@ -0,0 +1,10 @@
+T = int(input())
+
+for _ in range(T):
+    a = int(input())
+    b = int(input())
+    sum_ = 0
+    for n in range(a,b+1):
+        if pow(n,0.5) == int(pow(n,0.5)):
+            sum_ += n
+    print(f"Case {_+1}: {sum_}")
